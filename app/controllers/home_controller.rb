07adc8dc091e15a497bb274f48d316664b5780e2
Andre Quina <andre.quina@gmail.com>
2015-09-25 16:58:12 -0400

    show all until starburst issue is resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-  	@servers = Server.where({percent_passing: {"$gte" => 0}}).order_by("percent_passing"=>:desc)
+  	@servers = Server.all.order_by("percent_passing"=>:desc)
+  	# show all until issue is resolved
+  	#@servers = Server.where({percent_passing: {"$gte" => 0}}).order_by("percent_passing"=>:desc)
   end
 
 end
