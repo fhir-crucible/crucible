39e5928283ae6d29cd57f807d2a3a3ce8b83512f
Andre Quina <andre.quina@gmail.com>
2015-09-30 08:29:13 -0400

    show only servers with starburst on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-  	@servers = Server.all.order_by("percent_passing"=>:desc)
+  	#@servers = Server.all.order_by("percent_passing"=>:desc)
   	# show all until issue is resolved
-  	# @servers = Server.where({percent_passing: {"$gte" => 0}}).order_by("percent_passing"=>:desc)
+  	@servers = Server.where({percent_passing: {"$gte" => 0}}).order_by("percent_passing"=>:desc)
   end
 
 end
