1af3f6e03048fd5333a4ca680a373f718e6e9e91
Andre Quina <andre.quina@gmail.com>
2015-11-24 14:48:47 -0500

    fix front end tests

--- spec/javascripts/magic_lamp.rb
+++ spec/javascripts/magic_lamp.rb
@@ -1,6 +1,7 @@
 MagicLamp.define(controller: ServersController) do
   fixture do # servers/show
     @server = Server.new(url: 'www.example.com')
+    @server.save!
     @tests = Test.all.sort {|l,r| l.name <=> r.name}
     render :show
   end  
