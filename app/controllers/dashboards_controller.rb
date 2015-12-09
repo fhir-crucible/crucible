9d5352454729046de58554fbe31c2a3d76a487dc
Andre Quina <andre.quina@gmail.com>
2015-12-09 12:40:15 -0500

    fix dashboard when the server has never been run successfully

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -15,7 +15,10 @@ class DashboardsController < ApplicationController
     servers.each do |server|
       suites.each do |suite|
         test_ids = suite.methods.map {|m| m['id']}
-        test_results = server.aggregate_run.results.select {|r| test_ids.include? r['id']}
+        test_results = []
+        if server.aggregate_run
+          test_results = server.aggregate_run.results.select {|r| test_ids.include? r['id']}
+        end
         results[server.id.to_s] ||= {}
         results[server.id.to_s][suite.id.to_s] = test_results
       end
