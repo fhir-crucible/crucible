f10024b7666ef29c4d3360a3fc78d69e071aa540
cdowling <cdowling@mitre.org>
2017-02-21 16:49:59 -0500

    fixed statistics model. Added function to increment stats when a test is run

--- app/models/test_run.rb
+++ app/models/test_run.rb
@@ -127,6 +127,8 @@ class TestRun
       # If somebody else picked up this job while I was working on this, don't save these results and leave
       return false if TestRun.find(self.id).worker_id != this_worker_id
 
+      increment_test_counter(val)
+
       self.last_updated = DateTime.now
       self.test_results << result
       self.status = 'complete' if self.test_results.length == self.tests.length
@@ -234,6 +236,10 @@ class TestRun
       results.last[:test_method] = test.name
     end
     results
+  end
+
+  def increment_test_counter(tests)
+    Statistics.all.update({ "$inc": {tests_run: tests.length } })
   end
 
 end
