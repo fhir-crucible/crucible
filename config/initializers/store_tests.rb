af5a17301048c992b55a4db1e2076bcba46986c1
Rob Scanlon <robscanlon@gmail.com>
2017-05-02 14:20:18 -0400

    Update load version to refresh db tests.

--- config/initializers/store_tests.rb
+++ config/initializers/store_tests.rb
@@ -1,5 +1,5 @@
 # date based version to force tests to reload on startup
-LOAD_VERSION=20170401
+LOAD_VERSION=20170402
 
 Test.any_of({:load_version.exists => false},{:load_version.lt => LOAD_VERSION}).delete
 
