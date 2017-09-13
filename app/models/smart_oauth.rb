904fb9b2aed560ccaa1f4925ddc2ae0445f17f9d
Mahalingam <kmahalingam@mitre.org>
2017-09-13 11:43:26 -0400

    Update urls to be class methods instead of fields

--- app/models/smart_oauth.rb
+++ app/models/smart_oauth.rb
@@ -1,8 +1,15 @@
 class SMARTOAuth
   include Mongoid::Document
+
   field :client_id, type: String, default: "00000000-0000-0000-0000-000000000000"
   field :scopes, type: String, default: "launch openid profile patient/*.read"
-  field :base_url, type: String, default: ""
-  field :redirect_url, type: String, default: "http://localhost:3000/smart/app"
+
+  def self.base_url
+    ""
+  end
+
+  def self.redirect_url
+    "http://localhost:3000/smart/app"
+  end
 
 end
